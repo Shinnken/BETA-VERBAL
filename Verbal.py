d83ce8b9f39fe59c305d13c2d55b42a9c588ebce
ksuchodo <01159102@pw.edu.pl>
2023-03-09 23:36:16 +0100

    Minor improvement.

--- Verbal.py
+++ Verbal.py
@@ -10,24 +10,25 @@ options.add_argument(r"user-data-dir=C:\Users\Shinken\AppData\Local\Google\Chrom
 options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 1,
   })
-driver = webdriver.Chrome(service=Service(r'C:\Users\Shinken\Documents\BETA\chromedriver_win32\chromedriver.exe'), options=options)
-driver.get('https://dictation.io/speech')
-driver.minimize_window()
-select = Select(driver.find_element("xpath", "//select[@id='lang']"))
+service = Service(r'C:\Users\Shinken\Documents\BETA\chromedriver_win32\chromedriver.exe')
+stt = webdriver.Chrome(service=service, options=options)
+stt.get('https://dictation.io/speech')
+stt.minimize_window()
+select = Select(stt.find_element("xpath", "//select[@id='lang']"))
 select.select_by_value('pl-pl')
 time.sleep(1)
-driver.find_element("xpath", "//a[@class='btn-mic btn btn--primary-1']").click()
+stt.find_element("xpath", "//a[@class='btn-mic btn btn--primary-1']").click()
 time.sleep(4)
-textbox = driver.find_element("xpath", "//div[@class='ql-editor ql-blank']")
+textbox = stt.find_element("xpath", "//div[@class='ql-editor ql-blank']")
 
 while True:
     if textbox.text != '':
         if textbox.text.strip() == 'koniec':
             textbox.clear()
-            driver.quit()
+            stt.quit()
             print('Koniec1')
             exit()
         print(textbox.text)
         textbox.clear()
 
-#driver.quit()
+# stt.quit()
